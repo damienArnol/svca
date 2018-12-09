2828d73759e31f402c68018a939e17e35932cf72
damien.arnol <damien.arnol@gmail.com>
2018-12-09 11:09:26 +0000

    minor changes

--- SVCA/svca/run/call_run_indiv.sh
+++ SVCA/svca/run/call_run_indiv.sh
@@ -1,3 +1,12 @@
 data_dir='../../examples/IMC_example_local/Cy1x7/'
 
+# optional arguments
+# protein_index=''
+# normalisation=''
+# outdir=''  # to select a different output dir fron the indir/results
+
 python run_indiv.py --indir $data_dir
+
+# --outdir $outdir
+# --protein_index $protein_index
+# --normalisation $normalisation
